c4d78f6562152a12a9217ebdda156b17cb57a5f6
Raymond Trinh <RaymondTrinh91@gmail.com>
2019-10-21 14:00:22 -0700

    Added "AddTodo" component

--- 30-before-30/src/Components/AddTodo.js
+++ 30-before-30/src/Components/AddTodo.js
@@ -1,11 +1,58 @@
-import React from "react";
+import React, { useState } from "react";
+import { withFormik, Form, Field } from "formik";
+import * as Yup from "yup";
 
-const AddTodo = () => {
+const AddTodo = (touched, errors, status) => {
+    const [task, setTask] = useState([]);
+
+    useEffect(()=> {
+        status && setTask(task => [...task, status]);
+    },[status])
+    
     return (
-        <div>
-            
+        <div className="add-todo">
+            <Form>
+                <Field
+                    type="text"
+                    name="todo"
+                    placeholder="Add a Todo"
+                    />
+                {touched.todo && errors.todo && (
+                    <p className="todoReq">{errors.todo}</p>}
+                )}
+                <Field
+                    type="text"
+                    name="time"
+                    placeholder="Add a Deadline"
+                    />
+                {touched.time && errors.time && (
+                    <p className="todoReq">{errors.time}</p>}
+                )}
+                <button type="submit">Add Task</button>
+            </Form>
         </div>
     )
 }
+const FormikAddTodo = withFormik ({
+    mapPropsToValues({todo, time}){
+        return {
+            todo = todo || "",
+            time = time || ""
+        };
+    },
 
-export default AddTodo;
+    validationSchema: Yup.object().shape({
+        todo: Yup.string().required("Please add a Task"),
+        time: Yup.string().required("Please add a Deadline")
+    }),
+
+    handleSubmit(values, {resetForm, setStatus}) {
+        axios
+            .post("CHANGE ME I GUESS", values)
+            .then(res => { setStatus(res.data); })
+            .catch(error => console.log("Add Todo Error", error));
+        
+        resetForm();
+    }
+})(AddTodo);
+export default FormikAddTodo;
