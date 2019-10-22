f3561195979e0d3819e84a34e55d5b2d52afb361
Chelsea Wetzel <chelseagilmore14@gmail.com>
2019-10-22 13:12:11 -0400

    styles

--- 30-before-30/src/Components/TodoCard.js
+++ 30-before-30/src/Components/TodoCard.js
@@ -1,6 +1,6 @@
 import React from "react";
 import moment from "moment";
-import "./Component CSS/TodoCard.css";
+import "./ComponentCSS/TodoCard.css";
 
 const TodoCard = props => {
 let today = moment().format();
